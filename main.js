5725880e5c97642a156ecd6918914d0e6267ce84
Andrey <mozobretenie@gmail.com>
2023-10-06 22:53:50 +0300

    модальное и счетчик

--- main.js
+++ main.js
@@ -21,3 +21,26 @@ button_parent_minus.addEventListener("click", function(){
 })
 
 
+const button_child_plus = document.querySelector("#child-plus");
+const button_child_minus = document.querySelector("#child-minus");
+const child_input = document.querySelector("#child");
+
+button_child_plus.addEventListener("click", function(){
+  if(child_input.value < 10) {
+    child_input.value = ++child_input.value
+  }
+  else{
+    child_input.value = 10
+  }
+})
+
+button_child_minus.addEventListener("click", function(){
+  if(child_input.value > 1) {
+    child_input.value = --child_input.value
+  }
+  else{
+    child_input.value = 1
+  }
+})
+
+
